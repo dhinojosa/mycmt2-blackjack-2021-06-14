58ac21515d0c2a857b2ef4a1ad29479507cf0d9b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-06-14 12:51:42 -0600

    Lab 2 Done

---
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -0,0 +1,25 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.stream.Collectors;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class ConsoleHand {
+
+    private Hand hand;
+
+    public ConsoleHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    static String displayFirstCard(Hand hand) {
+        return ConsoleCard.display(hand.cards().get(0));
+    }
+
+    static void display(Hand hand) {
+        System.out.println(hand.cards().stream()
+                               .map(ConsoleCard::display)
+                               .collect(Collectors.joining(
+                                    ansi().cursorUp(6).cursorRight(1).toString())));
+    }
+}
