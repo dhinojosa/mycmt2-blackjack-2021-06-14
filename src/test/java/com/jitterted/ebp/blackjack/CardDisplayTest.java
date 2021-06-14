5791a672c8e300ab566e40f0593dad144b614001
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-06-14 12:13:12 -0600

    Lab 0 & 1 Console Card

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,24 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardDisplayTest {
+
+    @Test
+    void displayTenAsString() {
+        Card card = new Card(Suit.CLUBS, Rank.TEN);
+        String display = ConsoleCard.display(card);
+        assertThat(display)
+            .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+
+    @Test
+    void displayNonTenAsString() {
+        Card card = new Card(Suit.HEARTS, Rank.SEVEN);
+        String display = ConsoleCard.display(card);
+        assertThat(display)
+            .isEqualTo("[31m┌─────────┐[1B[11D│7        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        7│[1B[11D└─────────┘");
+    }
+}
